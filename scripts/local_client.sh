d5e81e5527cdf5a688627007b24725ebe9dff60f
samer-hamood <samer.hamood@gmail.com>
2026-01-02 15:07:11 +0000

    codecrafters submit [skip ci]

---
+++ scripts/local_client.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+REQUEST_FILE='scripts/request'
+
+input=$(tr -d '[:space:]' < "$REQUEST_FILE")
+
+# Print hex dump
+# echo -n "$input" | xxd -r -p | hexdump -C
+
+echo -n "$input" | xxd -r -p | nc localhost 9092 | hexdump -C
+
