f9194da2565de5cd8b8f7c8e7c18264009560413
samer-hamood <samer.hamood@gmail.com>
2026-01-12 21:41:03 +0000

    Replace field type from u8 to UnsignedVarint

--- src/tagged_fields_section.rs
+++ src/tagged_fields_section.rs
@@ -1,44 +1,53 @@
-use crate::{byte_parsable::ByteParsable, serializable::Serializable, size::Size};
+use crate::{
+    byte_parsable::ByteParsable, serializable::Serializable, size::Size,
+    types::unsigned_varint::UnsignedVarint,
+};
 
 pub const EMPTY: usize = 1;
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone)]
 pub struct TaggedFieldsSection {
-    number_of_tagged_fields: u8,
+    number_of_tagged_fields: UnsignedVarint,
 }
 
 impl TaggedFieldsSection {
-    pub fn empty() -> TaggedFieldsSection {
-        TaggedFieldsSection {
-            number_of_tagged_fields: 0,
+    pub fn empty() -> Self {
+        Self {
+            number_of_tagged_fields: UnsignedVarint::new(0),
+            // TODO: add field for tagged fields
         }
+    }
+}
+
+impl PartialEq for TaggedFieldsSection {
+    fn eq(&self, other: &Self) -> bool {
+        self.number_of_tagged_fields.value == other.number_of_tagged_fields.value
+        // TODO: compare tagged fields
     }
 }
 
 impl Size for TaggedFieldsSection {
     fn size(&self) -> usize {
-        if self.number_of_tagged_fields == 0 {
-            1
-        } else {
-            panic!("Calculating size of non-empty Tagsection not implemented")
-        }
+        self.number_of_tagged_fields.size()
+        // TODO: add sizes for tagged fields also
     }
 }
 
 impl Serializable for TaggedFieldsSection {
     fn to_be_bytes(&self) -> Vec<u8> {
         let mut bytes = Vec::new();
-        if self.number_of_tagged_fields == 0 {
-            bytes.push(0);
-        }
+        bytes.extend_from_slice(&self.number_of_tagged_fields.to_be_bytes());
+        // TODO: serialize tagged fields also
         bytes
     }
 }
 
 impl ByteParsable<TaggedFieldsSection> for TaggedFieldsSection {
-    fn parse(_bytes: &[u8], _offset: usize) -> TaggedFieldsSection {
-        // let mut offset = offset;
-        // TOOD: parse length to determine what to return: empty or not
-        TaggedFieldsSection::empty()
+    fn parse(bytes: &[u8], offset: usize) -> TaggedFieldsSection {
+        let number_of_tagged_fields = UnsignedVarint::parse(bytes, offset);
+        // TODO: parse tagged fields also
+        TaggedFieldsSection {
+            number_of_tagged_fields,
+        }
     }
 }
