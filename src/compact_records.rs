a68facc035bf38bc293fbc09e2fa3e91a103fd3e
samer-hamood <samer.hamood@gmail.com>
2026-01-13 00:16:29 +0000

    Add function to create CompactRecords from array of RecordBatch

--- src/compact_records.rs
+++ src/compact_records.rs
@@ -1,3 +1,4 @@
+use crate::records::record_batch::RecordBatch;
 use crate::serializable::{BoxedSerializable, Serializable};
 use crate::size::Size;
 use crate::tagged_fields_section::TaggedFieldsSection;
@@ -15,6 +16,25 @@ impl CompactRecords {
         Self {
             records: CompactNullableBytes::null(),
         }
+    }
+
+    pub fn from_record_batches(record_batches: &[RecordBatch]) -> Self {
+        let record_batches_bytes = record_batches
+            .iter()
+            .flat_map(|record_batch| record_batch._parsed_bytes.clone())
+            .collect();
+        let unsigned_varint_value = record_batches
+            .iter()
+            .map(|record_batch| record_batch.expected_length() as u32)
+            .sum::<u32>()
+            + 1;
+        let length = UnsignedVarint::new(unsigned_varint_value);
+        let bytes = if record_batches.is_empty() {
+            None
+        } else {
+            Some(record_batches_bytes)
+        };
+        Self::new(length, bytes)
     }
 
     fn new(length: UnsignedVarint, bytes: Option<Vec<u8>>) -> Self {
