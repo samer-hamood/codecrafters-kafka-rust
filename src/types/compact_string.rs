74b8eef9794dd379e5337ed3726233d30a26ad2b
samer-hamood <samer.hamood@gmail.com>
2026-01-30 18:11:47 +0000

    Put default Debug implementation back in CompactString

--- src/types/compact_string.rs
+++ src/types/compact_string.rs
@@ -7,7 +7,7 @@ use crate::types::unsigned_varint::UnsignedVarint;
 
 // https://kafka.apache.org/27/protocol.html#protocol_types
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct CompactString {
     pub length: UnsignedVarint,
     pub bytes: Option<Vec<u8>>,
@@ -48,16 +48,6 @@ impl Serializable for CompactString {
         }
     }
 
-impl Debug for CompactString {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        if let Some(bytes) = &self.bytes {
-            match str::from_utf8(bytes) {
-                Ok(s) => write!(f, "{}", s),
-                Err(e) => panic!("Invalid UTF-8: {}", e),
-            }
-        } else {
-            panic!("No bytes to display CompactString")
-        }
     }
 }
 
