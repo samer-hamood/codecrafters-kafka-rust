8fbd4b8dece854ab2952b81a64dff333edf5d31d
samer-hamood <samer.hamood@gmail.com>
2026-01-30 17:56:29 +0000

    Print command

--- scripts/local_client.sh
+++ scripts/local_client.sh
@@ -6,6 +6,6 @@ input=$(tr -d '[:space:]' < "$REQUEST_FILE")
 
 # Print hex dump
 # echo -n "$input" | xxd -r -p | hexdump -C
-
+echo -e "Running command: echo -n \"$input\" | xxd -r -p | nc localhost 9092 | hexdump -C\n"
 echo -n "$input" | xxd -r -p | nc localhost 9092 | hexdump -C
 
