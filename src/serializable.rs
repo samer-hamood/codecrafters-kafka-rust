4670a8746c11ca70b8965d5a921ccb0ca48a5994
samer-hamood <samer.hamood@gmail.com>
2026-01-30 17:55:38 +0000

    Ensure implementers of Serializable also be a Size type

--- src/serializable.rs
+++ src/serializable.rs
@@ -1,6 +1,8 @@
+use crate::size::Size;
+
 pub type BoxedSerializable = Box<dyn Serializable>;
 
-pub trait Serializable {
+pub trait Serializable: Size {
     fn to_be_bytes(&self) -> Vec<u8> {
         self.serializable_fields()
             .iter()
